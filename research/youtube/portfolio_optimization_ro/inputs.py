7327f77dc96d4c9f7a2853cc26325e8de7404f56
myko_d <100625110+pymd661@users.noreply.github.com>
2025-09-30 22:22:53 -0400

    Add files via upload

---
+++ research/youtube/portfolio_optimization_ro/inputs.py
@@ -0,0 +1,86 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+from scipy.optimize import minimize
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+
+### Define Tickers
+tickers = ['SPY','BND','GLD','QQQ','VTI']
+
+### Define the time range
+end_date = datetime.today()
+start_date = '2007-04-03'
+
+### Get the adjusted close price
+adj_close_df = pd.DataFrame() 
+for ticker in tickers: 
+    data = yf.download(ticker, start = start_date,end = end_date, auto_adjust=True) 
+    adj_close_df[ticker] = data['Close']
+
+### Calculate log returns
+log_returns = np.log(adj_close_df/adj_close_df.shift(1))
+# drop NAs
+log_returns = log_returns.dropna()
+
+### Calculate covarinace
+cov_matrix = log_returns.cov() * 252 # annualized
+
+
+### Define metrics
+def standard_deviation(weights, cov_matrix):
+    variance = weights.T @ cov_matrix @ weights
+    return np.sqrt(variance)
+
+def expected_return(weights, log_returns):
+    return np.sum(log_returns.mean()*weights)*252
+
+def sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate):
+    return (expected_return(weights, log_returns) - risk_free_rate) / standard_deviation(weights, cov_matrix)
+
+### Optimization
+
+### Get the risk free rate
+risk_free_rate = .02
+
+def neg_sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate):
+    return -sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate)
+
+constraints = {'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}
+bounds = [(0, 0.8) for _ in range(len(tickers))]
+initial_weights = np.array([1/len(tickers)]*len(tickers))
+
+optimized_results = minimize(neg_sharpe_ratio, initial_weights, args=(log_returns, cov_matrix, risk_free_rate), method='SLSQP', constraints=constraints, bounds=bounds)
+
+optimal_weights = optimized_results.x
+
+print("Optimal Weights:")
+for ticker, weight in zip(tickers, optimal_weights):
+    print(f"{ticker}: {weight:.4f}")
+
+optimal_portfolio_return = expected_return(optimal_weights, log_returns)
+optimal_portfolio_volatility = standard_deviation(optimal_weights, cov_matrix)
+optimal_sharpe_ratio = sharpe_ratio(optimal_weights, log_returns, cov_matrix, risk_free_rate)
+
+print(f"Expected Annual Return: {optimal_portfolio_return:.4f}")
+print(f"Expected Volatility: {optimal_portfolio_volatility:.4f}")
+print(f"Sharpe Ratio: {optimal_sharpe_ratio:.4f}")
+
+
+
+
+plt.figure(figsize=(10, 6))
+plt.bar(tickers, optimal_weights)
+
+plt.xlabel('Assets')
+plt.ylabel('Optimal Weights')
+plt.title('Optimal Portfolio Weights')
+
+plt.show()
+
+
+
+
