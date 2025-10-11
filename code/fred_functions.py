09f943752a5e3564ea52fa83c26a185d8e726c9d
pythonmd661 <pythonmd661@gmail.com>
2025-10-11 17:57:54 -0400

    updated ipynb for etf returns and gdp

--- code/fred_functions.py
+++ code/fred_functions.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 """Function to clean up fred data"""
 
-def yoy_change(x):#yoy change
-    return x.pct_change(12)
+def yoy_change(df):#yoy change
+    return df.pct_change(12)
 
-def mom_change(x):#month over month change
-    return x.pct_change(1)
+def mom_change(df):#month over month change
+    return df.pct_change(1)
 
-def forward_fill_monthly(x):
-    x = x.copy()
+def forward_fill(df):
+    x = df.copy()
     x.index = pd.to_datetime(x.index)
-    return x.resample("M").ffill()
+    x = x.ffill()
+    return x.ffill()
+
+def resample_me(df):
+    return df.resample("ME").last()
+
+
