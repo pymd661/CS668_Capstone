1fd0790ebef92bc935af7fd48f501f09f689f951
pythonmd661 <pythonmd661@gmail.com>
2025-10-13 15:13:48 -0400

    added credit build

--- code/fred_functions.py
+++ code/fred_functions.py
@@ -34,6 +34,44 @@ def rolling_3m(df,columns_to_process):
 def rename_columns(df,columns_dictionary):
     return df.rename(columns=columns_dictionary,inplace=True)
 
+def credit_spread(df,lag_months,delta_calc=False):
+    rate_col =  ['Mort30Y', 'UST10Y', 'UST2Y', 'UST3M', 'TIPS10Y','CorpBAA', 'CorpAAA', 'FedFundsRate']
+    # result_col,col1_input,col2_input
+    spread_logic = [
+        ('Slope_10Y_2Y',"UST10Y","UST2Y"),
+        ('Slope_10Y_3M',"UST10Y","UST3M"),
+        ('Slope_10Y_FF',"UST10Y","FedFundsRate"),
+        ('InfExp_10Y',"UST10Ym","TIPS10Y"),
+        ('BAA_minus_10Y','CorpBAA','UST10Y'),
+        ('AAA_minus_10Y','CorpAAA','UST10Y'),
+        ('BAA_minus_AAA','CorpBAA','CorpAAA')
+    ]
+    check_col = [col for col in rate_col if col in df.columns]
+    if not check_col:
+        return pd.DataFrame()
+    m = df[check_col].copy()
+    m.index=pd.to_datetime(m.index)
+    X = m.resample("ME").last()
+
+    for result_col,col1_input,col2_input in spread_logic:
+        if col1_input in X.columns and col2_input in X.columns:
+            X[result_col] = X[col1_input]-X[col2_input]
+    
+    if delta_calc==True:
+        cols_to_diff = X.columns.copy()
+        for period in [1,3]:
+            X=X.assign(**{
+                f'{col}_Delta{period}m': X[col].diff(period) 
+                for col in cols_to_diff
+            })
+    else:
+        pass
+    
+    if lag_months > 0:
+        X =X.shift(lag_months)
+    return X.dropna(how='all')
+
+
 
 
 
