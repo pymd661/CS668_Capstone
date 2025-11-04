7bb7cd7cbd0b06acdb6b2d89be09042a42d47182
pythonmd661 <pythonmd661@gmail.com>
2025-11-04 15:16:51 -0500

    updated comments, more details

--- code/index_prices.py
+++ code/index_prices.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 
 def cummulative_return(df,columns_to_process,rolling_value=3,comp_freq=4):
+    'calculates the cumulative return, user has to give columns, rolling window, and compounding frequency'
+    
     df_mod = df.copy()
     for col in columns_to_process:
-        cummulative_3m = (1+df_mod[col]).rolling(rolling_value).apply(np.prod, raw=True) - 1
-        df_mod[f'{col}_{rolling_value}m'] = cummulative_3m
-        cummulative_3m_ann = (1+cummulative_3m)**comp_freq-1
-        df_mod[f'{col}_{rolling_value}m_ann'] = cummulative_3m_ann
-        sd_3m = df_mod[col].rolling(rolling_value).std()
-        df_mod[f'{col}_3m_sd'] = sd_3m
-        df_mod[f'{col}_3m_ann_sd'] = sd_3m * np.sqrt(comp_freq)
+        cummulative_3m = (1+df_mod[col]).rolling(rolling_value).apply(np.prod, raw=True) - 1 # calculates a growth factor based on a rolling period
+        df_mod[f'{col}_{rolling_value}m'] = cummulative_3m # creates rolling cumulative return column
+        cummulative_3m_ann = (1+cummulative_3m)**comp_freq-1 # annualized
+        df_mod[f'{col}_{rolling_value}m_ann'] = cummulative_3m_ann # creates annualized column
+        sd_3m = df_mod[col].rolling(rolling_value).std() # calculates the std
+        df_mod[f'{col}_3m_sd'] = sd_3m # creates std column
+        df_mod[f'{col}_3m_ann_sd'] = sd_3m * np.sqrt(comp_freq) # creates the annualized std/volatility
 
     return df_mod
 
